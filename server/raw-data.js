6bd39403b1986386e98ac41ff0864eb8211b9eac
Sundaisy <daisyzs1213@163.com>
2016-09-09 11:43:32 +0800

    add raw-data & .gitignore

---
+++ server/raw-data.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zhangsha on 16-9-9.
+ */
+module.exports = [
+  {
+    Student: [{
+      student_id: '06131001',
+      name: '小白',
+      sex: '女',
+      chinese: 88,
+      math: 99
+    },
+    {
+      student_id: '06131002',
+      name: '小黑',
+      sex: '男',
+      chinese: 60,
+      math: 99
+    },
+    {
+      student_id: '06131001',
+      name: '大白',
+      sex: '女',
+      chinese: 88,
+      math: 79
+    }]
+  }
+];
