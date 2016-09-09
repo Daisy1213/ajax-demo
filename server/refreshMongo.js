c010c6541d992c4f779b1bf08a594ce175564879
Sundaisy <daisyzs1213@163.com>
2016-09-09 11:56:11 +0800

    add refreshData

---
+++ server/refreshMongo.js
@@ -0,0 +1,27 @@
+/**
+ * Created by zhangsha on 16-9-9.
+ */
+let mongoose = require('mongoose');
+let rawData = require('./raw-data');
+let Student = require('./Student');
+
+mongoose.connect('mongodb://localhost/student');
+
+const modelMap = {
+  Student
+};
+
+let docs = Object.keys(rawData);
+
+Object.keys(rawData)
+  .forEach(v => {
+    modelMap[v].remove(() => {
+      modelMap[v].create(rawData[v], () => { //每次插入一个表，然后docs这个数组的表名就少一个，
+        docs = docs.filter(doc => doc !== v);
+        if (docs.length === 0) { // 直到docs长度为零，即数据刷新完毕
+          console.log('All data refreshed!');
+          process.exit(0);
+        }
+      })
+    })
+  })
